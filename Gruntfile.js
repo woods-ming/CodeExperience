b830a28158be4f48f7227b08ff748ade0221be63
woods-ming <woodsming@sina.com>
2015-03-19 17:03:10 +0800

    修改grunt的任务将content文件夹拷贝到dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -290,7 +290,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'content/{,*/}*.*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
